7b21ef33d631960b37785976b9bc75b609f27ead
tylerb1 <tberb1290@gmail.com>
2024-07-10 14:15:59 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+## Data overlay example
+
+Add a data overlay to a map via React and use controls to visualize it differently.
+
+![Demo gif](https://i.imgur.com/FiqNRfZ.gif)
+
+### Run it
+
+    git clone https://github.com/mapbox/mapbox-react-examples.git
+
+    cd mapbox-react-examples/data-overlay
+    npm install
+    npm start
+
+    open http://localhost:3000/
+
+### Access token
+
+    mapboxgl.accessToken = // YOUR_API_KEY_HERE
+
+Replace the [API key](https://docs.mapbox.com/help/getting-started/access-tokens/) in `src/Map.js`.
+
+Runs the app from http://localhost:3000
+
+Data provided by [Natural Earth](http://www.naturalearthdata.com/).
---
+++ src/Map.js
@@ -0,0 +1,50 @@
+import React, { useRef, useEffect } from 'react';
+import './Map.css';
+import data from './parcels.geojson';
+import mapboxgl from 'mapbox-gl';
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API_KEY;
+
+const Map = () => {
+  const mapContainerRef = useRef(null);
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-89.401230, 43.073051],
+      zoom: 12,
+      pitch: 60.00,
+    });
+
+    map.on('load', () => {
+      map.addSource('taxes', {
+        type: 'geojson',
+        data
+      });
+
+      map.addLayer(
+        {
+          id: "taxes",
+          source: "taxes",
+          type: "fill-extrusion",
+          paint: {
+            "fill-extrusion-color": ["interpolate", ['linear'], ["/", ["get", "NetTaxes"], ["get", "LotSize"]], 0, "green", 10, "red"],
+            "fill-extrusion-height": ["*", 100, ["/", ["get", "NetTaxes"], ["get", "LotSize"]]],
+            "fill-extrusion-base": 0,
+            "fill-extrusion-opacity": 0.6
+          }
+        }
+      );
+    });
+
+    return () => map.remove();
+  }, []);
+
+  return (
+    <div>
+      <div ref={mapContainerRef} className='map-container' />
+    </div>
+  );
+};
+
+export default Map;
